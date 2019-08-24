7112bc8ac0b9fa0f5c7eeddb08b3d7e4b24973cd
Steven Carter <stevenca@cisco.com>
2019-08-24 15:22:13 -0400

    Made more better

--- play.sh
+++ play.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
+OPTIONS=""
+if [[ ! -z "$ANSIBLE_VAULT_PASSWORD_FILE" ]]; then
+   OPTIONS="--env ANSIBLE_VAULT_PASSWORD_FILE=/tmp/vault.pw -v $ANSIBLE_VAULT_PASSWORD_FILE:/tmp/vault.pw"
+fi
 
-docker run -it --rm -v $PWD:/ansible -v /tmp:/tmp --env PWD="/ansible" --env USER="$USER" --env VIRL_SESSION="$VIRL_SESSION" --env VIRL_HOST="$VIRL_HOST" --env VIRL_USERNAME="$VIRL_USERNAME" --env VIRL_PASSWORD="$VIRL_PASSWORD" ansible-viptela ansible-playbook "$@" 
+if [[ ! -z "$VIRL_HOST" ]]; then
+   OPTIONS="$OPTIONS --env VIRL_SESSION=$VIRL_HOST"
+fi
+if [[ ! -z "$VIRL_USERNAME" ]]; then
+   OPTIONS="$OPTIONS --env VIRL_SESSION=$VIRL_USERNAME"
+fi
+if [[ ! -z "$VIRL_PASSWORD" ]]; then
+   OPTIONS="$OPTIONS --env VIRL_SESSION=$VIRL_PASSWORD"
+fi
+if [[ ! -z "$VIRL_SESSION" ]]; then
+   OPTIONS="$OPTIONS --env VIRL_SESSION=$VIRL_SESSION"
+fi
+
+OPTIONS="$OPTIONS --env ANSIBLE_ROLES_PATH=/ansible/roles"
+
+docker run -it --rm -v $PWD:/ansible --env PWD="/ansible" --env USER="$USER" $OPTIONS ansible-viptela ansible-playbook "$@"
