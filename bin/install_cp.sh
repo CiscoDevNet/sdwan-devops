39b7a61d37ea62cf36a6831881644b24721d155c
Lori Jakab <lojakab@cisco.com>
2022-11-22 16:03:26 +0100

    AWS: edge onboarding progress

--- bin/install_cp.sh
+++ bin/install_cp.sh
@@ -4,4 +4,4 @@ set -o pipefail
 
 ./play.sh /ansible/day_0/build-control-plane.yml
 ./play.sh /ansible/day_0/config-sdwan.yml
-./play.sh /ansible/day_1/config-sdwan.yml
+#./play.sh /ansible/day_1/config-sdwan.yml
