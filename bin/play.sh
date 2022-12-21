95dd030056715df9b5e8688b5544523d5f675388
Lori Jakab <lojakab@cisco.com>
2022-12-21 20:16:44 +0100

    Fix superfluous backtick in play.sh

--- bin/play.sh
+++ bin/play.sh
@@ -97,7 +97,7 @@ docker run -it --rm -v $PROJ_ROOT/ansible:/ansible \
    -v $PROJ_ROOT/terraform-sdwan:/terraform-sdwan \
    -v $PROJ_ROOT/sdwan-edge:/sdwan-edge \
    `# Uncomment the following line if you are using a container image with the Azure CLI included and want to deploy a cEdge on Azure` \
-   `#-v $HOME/.azure:/root/.azure`` \
+   `#-v $HOME/.azure:/root/.azure` \
    --env PWD="/ansible" \
    --env USER="$USER" \
    --env VMANAGE_ORG="$VMANAGE_ORG" \
