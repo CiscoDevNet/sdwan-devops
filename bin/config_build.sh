e5614c44830898e36f08867d5cc4c8da6efc164c
Lori Jakab <lojakab@cisco.com>
2023-12-11 12:22:21 +0100

    Update OPA Conftest 0.46.0 --> 0.47.0

--- bin/config_build.sh
+++ bin/config_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CONFTEST_VERSION=${CONFTEST_VERSION:-v0.46.0}
+CONFTEST_VERSION=${CONFTEST_VERSION:-v0.47.0}
 
 set -o pipefail
 
--- bin/conftest.sh
+++ bin/conftest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-CONFTEST_VERSION=${CONFTEST_VERSION:-v0.46.0}
+CONFTEST_VERSION=${CONFTEST_VERSION:-v0.47.0}
 
 docker run --rm -v $PROJ_ROOT/config:/project openpolicyagent/conftest:${CONFTEST_VERSION} test config.yaml -d policy/data.yaml
